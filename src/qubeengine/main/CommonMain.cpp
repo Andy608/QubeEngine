30a6f269e871fbd168281bc1e735d702c6df8299
Andrew Rimpici <AndrewR608@gmail.com>
2020-02-14 04:34:07 -0500

    Added in glfw and vulkan support. Added ability to toggle between building QubeEngine as an executable or as a library

--- src/qubeengine/main/CommonMain.cpp
+++ src/qubeengine/main/CommonMain.cpp
@@ -5,9 +5,5 @@ extern int QubeEngineMain(void);
 int main(int argc, char* argv[])
 {
 	std::cout << "Common Main Entrance." << std::endl;
-	std::string* s = new std::string("hello, world!");
-	delete s;
-	std::cin.get();
-
 	return QubeEngineMain();
 }
--- src/qubeengine/main/QubeEngineMain.cpp
+++ src/qubeengine/main/QubeEngineMain.cpp
@@ -1,22 +1,39 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
-
-#include <iostream>
-#include <nanogui/nanogui.h>
-
 #include <qubeengine/core/QubeEngine.h>
 
-#include <matrix.hpp>
-#include <spdlog/logger.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
 
 int QubeEngineMain()
 {
 	std::cout << "Hello, Qube Engine." << std::endl;
-	std::cin.get();
 
-	qe::QubeEngine::constructEngine();
+    glfwInit();
 
-	glm::mat4 m;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
 
 	return 0;
 }
